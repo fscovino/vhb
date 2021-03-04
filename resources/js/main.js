6b8f1f7a7a920f7c35312d2d56398d7d5bb6cf6a
ErmiasTsege <71066745+ErmiasTsege@users.noreply.github.com>
2021-03-04 18:25:50 -0500

    VHB project-Ermias Commit

---
+++ resources/js/main.js
@@ -0,0 +1,44 @@
+
+const alertName = document.querySelector("#nameError");
+alertName.style.display = 'none';
+const alertEmail = document.querySelector("#emailError");
+alertEmail.style.display = 'none';
+const alertMessage = document.querySelector("#messageError");
+alertMessage.style.display = 'none';
+
+
+//Get the button tag and assing it to variable button
+const button = document.querySelector('#submit');
+
+//Validation function 
+function formValidation(e) {
+  //input full name
+  const nameInput = document.querySelector('#name').value;
+  //input field for email
+  const emailInput = document.querySelector('#email').value;
+  //input field for message
+  const messageInput = document.querySelector('#comments').value;
+
+ e.preventDefault(); //prevent default behaviour of the form
+
+    if (nameInput === "") {
+      alertName.style.display = 'block';
+ 
+    }
+    if (emailInput === "") {
+      alertEmail.style.display = 'block';
+    }
+    if (messageInput === "") {
+      alertMessage.style.display = 'block';
+    }
+   
+}
+
+button.addEventListener('click', formValidation);
+
+function removemessage(input){
+
+    input.style.display = 'none'
+}
+const nameInput = document.querySelector('#name').value;
+document.querySelector('#name').addEventListener ('click', removemessage(nameInput));
