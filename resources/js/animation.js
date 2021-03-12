f6800b5558cbe1b1d4e07b39985f6ea3d680f5ba
Francisco Scovino <franciscoscovino@gmail.com>
2021-03-12 10:16:27 -0500

    second draft

---
+++ resources/js/animation.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal();
+
+/* Navigation bar */
+sr.reveal('.navbar', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-80px'
+});
+
+/* Intersect Logo */
+sr.reveal('.animate-right', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-800px'
+});
+
+/* Where Data and Results Converge */
+sr.reveal('.animate-left', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-1800px'
+});
+
+/* Download Intersect white paper */
+sr.reveal('.animate-up', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-1000px'
+});
+
+/* Building Image */
+sr.reveal('.animate-appear', {
+    duration: 3000,
+    opacity: 0.15
+});
